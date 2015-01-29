571b39d1be1dc30ed61030b2fed95aaa5846fac0
kheld54 <kheld@performancesystemsgroup.com>
2015-01-29 14:52:51 -0500

    Sass Changes
    Move processing to grunt, output is now program.css and not custom.css.
    Update .gitingonre

--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,84 @@
 module.exports = function (grunt) {
 require('load-grunt-tasks')(grunt);
  grunt.initConfig({  
-        pkg: grunt.file.readJSON('package.json'),
-		useminPrepare: {
-	      html: 'www/indexweb.html',
-	      options: {
-		    flow: {
-				steps: {
-					js: ['concat','uglifyjs'],
-					jsmin: ['concat'],
-					css: ['concat', 'cssmin']
-				},
-				post: []
-			},
-	        dest: 'dist/'
-	      }
-	  },
-	  copy:{
-	     files: 
-            {
-                expand: true,
-                src: [
-                    'www/img/*.*',
-					'www/fonts/*.*',
-					'www/css/images/**',
-					'www/css/custom.css',
-					'www/js/custom.js',
-					'www/js/jquery-2.1.1.min.js',
-                    'www/*.html'
-                ],
-                dest: 'dist'
+       pkg: grunt.file.readJSON('package.json'),
+       
+            sass: {
+                dist: {
+                    files: {
+                        './www/css/program.css' : './sassCssFiles/program.scss'
+                    }
+                }
+	    },
+            
+            watch: {
+                    css: {
+                            files: '**/*.scss',
+                            tasks: ['sass']
+                    }
             },
-	    index: {
-	    	src: './www/indexweb.html', dest: 'dist/www/index.html'
-	    }
-	  },
-	   filerev: {
-    options: {
-      algorithm: 'md5',
-      length: 8
-    },
-    images: {
-      src: 'dist/**/all*.{css,js}'
-    }
-  },
-  clean: {
-    release: [
-        'dist'
-    ]
-},
-	  usemin:{
-	  	html:['dist/www/index.html']
-	  }
+            
+	    useminPrepare: {
+                html: 'www/indexweb.html',
+                options: {
+                    flow: {
+                        steps: {
+                            js: ['concat','uglifyjs'],
+                            jsmin: ['concat'],
+                            css: ['concat', 'cssmin']
+                        },
+                        post: []
+                    },
+                    dest: 'dist/'
+                }
+            },
+            copy:{
+               files: 
+                {
+                    expand: true,
+                    src: [
+                        'www/img/*.*',
+                        'www/fonts/*.*',
+                        'www/css/images/**',
+                        'www/css/custom.css',
+                        'www/js/custom.js',
+                        'www/js/jquery-2.1.1.min.js',
+                        'www/*.html'
+                    ],
+                    dest: 'dist'
+                },
+                index: {
+                    src: './www/indexweb.html', dest: 'dist/www/index.html'
+                }
+            },
+            filerev: {
+                options: {
+                  algorithm: 'md5',
+                  length: 8
+                },
+                images: {
+                  src: 'dist/**/all*.{css,js}'
+                }
+            },
+        clean: {
+          release: [
+              'dist'
+            ]
+        },
+	usemin:{
+	    html:['dist/www/index.html']
+	}
 });
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+
+grunt.registerTask('watchSass', [
+    'watch'                     
+]);
 
 grunt.registerTask('build', [
   'clean',
+  'sass',
   'useminPrepare',
   'concat:generated',
   'cssmin:generated',
