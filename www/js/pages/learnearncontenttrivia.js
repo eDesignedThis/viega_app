cca1a3a8e0e1ef0828953c6b0af9df1763aaad3a
Sheldon Bradley <sbradley@isicorporate.com>
2015-10-19 09:44:18 -0400

    Initial set-up for the Learn and Earn trivia daily question page.

--- www/js/pages/learnearncontenttrivia.js
+++ www/js/pages/learnearncontenttrivia.js
@@ -1,5 +1,35 @@
 function page_learn_earn_content_trivia_show(){
-        
+		// Add your JSON here 
+// 		getJson("", function(data) {
+// 		
+// 		var listString = '';
+// 		$.each(data, function (index, value) {
+// 			  listString += '<li data-psg-divider="' + learn and earn title+ '"> \
+// 					<div>' + trivia question Number here + '</div> \
+// 					<div>\
+// 						<span>' + trivia question goes here + '</span>\
+// 					</div>\
+// 					<div>'+ trivia button container here +'</div> \
+// 					<div>'+ trivia pager container here +'</div> \
+// 				</li>';
+// 		});
+// 
+// 		var ul = $('#psg-listview-psg-listview-learntrivia');
+// 		ul.html(listString);
+// 		ul.listview({
+// 			  autodividers: true,
+// 			  dividerTheme: "a" ,
+// 			  autodividersSelector: function (li) {
+// 				  var out = li.attr("data-psg-divider");
+// 				  return out;
+// 			  }
+// 		});
+// 		
+// 		ul.listview('refresh');
+// 		
+// 	  });
+	
+       
 }
 
  
--- www/js/pages/learnearndetail.js
+++ www/js/pages/learnearndetail.js
@@ -1,37 +1,37 @@
 function page_learn_earn_detail_show(){
 		
 	// Add your JSON here 
-	getJson("", function(data) {
-		
-		var listString = '';
-		$.each(data, function (index, value) {
-			  listString += '<li data-psg-divider="' + learn and earn title+ '"> \
-					<div>' + the detail info about THIS learn and earn here + '</div> \
-					<div>\
-						<span>' + learn and earn Type here + '</span>\
-						<span>' + Number OF Questions Here + '</span>\
-						<span>' + Learn and Earn ward ammount here + '</span>\
-					</div>\
-					<div> <a href="" data-role="button" >Start Now</a> </div> \
-				</li>';
-		});
-
-		var ul = $('#psg-listview-learndetail');
-		ul.html(listString);
-		ul.listview({
-			  autodividers: true,
-			  dividerTheme: "a" ,
-			  autodividersSelector: function (li) {
-				  var out = li.attr("data-psg-divider");
-				  return out;
-			  }
-		});
-		
-		ul.listview('refresh');
-		
-	  });
-	
-       
+// 	getJson("", function(data) {
+// 		
+// 		var listString = '';
+// 		$.each(data, function (index, value) {
+// 			  listString += '<li data-psg-divider="' + learn and earn title+ '"> \
+// 					<div>' + the detail info about THIS learn and earn here + '</div> \
+// 					<div>\
+// 						<span>' + learn and earn Type here + '</span>\
+// 						<span>' + Number OF Questions Here + '</span>\
+// 						<span>' + Learn and Earn ward ammount here + '</span>\
+// 					</div>\
+// 					<div> <a href="" data-role="button" >Start Now</a> </div> \
+// 				</li>';
+// 		});
+// 
+// 		var ul = $('#psg-listview-learndetail');
+// 		ul.html(listString);
+// 		ul.listview({
+// 			  autodividers: true,
+// 			  dividerTheme: "a" ,
+// 			  autodividersSelector: function (li) {
+// 				  var out = li.attr("data-psg-divider");
+// 				  return out;
+// 			  }
+// 		});
+// 		
+// 		ul.listview('refresh');
+// 		
+// 	  });
+// 	
+//        
 
 	
        
