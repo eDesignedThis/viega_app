323ee45bd034300030ab8190e817bd903ce4113b
Sheldon Bradley <sbradley@isicorporate.com>
2015-10-20 08:26:23 -0400

    Added the learn and earn review page html and javascript files.

---
+++ www/js/pages/learnearnreview.js
@@ -0,0 +1,34 @@
+function page_learn_earn_main_review() {
+	// Add your JSON here 
+// 	getJson("", function(data) {
+// 		
+// 		var listString = '';
+// 		$.each(data, function (index, value) {
+// 			  listString += '<li data-psg-divider="' + learn and earn title+ '"> \
+// 					<div>' + Thank you or completion message + '</div> \
+// 					<div>\
+// 						<span>' + results of quiz or survey + '</span>\
+// 						<span>' + The completion date + '</span>\
+// 					</div>\
+// 					<div> <a href="" data-role="button" >back to learn and earn</a> </div> \
+// 				</li>';
+// 		});
+// 
+// 		var ul = $('#psg-listview-learnreview');
+// 		ul.html(listString);
+// 		ul.listview({
+// 			  autodividers: true,
+// 			  dividerTheme: "a" ,
+// 			  autodividersSelector: function (li) {
+// 				  var out = li.attr("data-psg-divider");
+// 				  return out;
+// 			  }
+// 		});
+// 		
+// 		ul.listview('refresh');
+// 		
+// 	  });
+// 	
+//    
+}
+
