9708594ccbc02b3b82567d232a1da823eaa7ed33
Sheldon Bradley <sbradley@isicorporate.com>
2015-10-21 15:18:20 -0400

    Updates for the learn and earn review page.

--- www/js/pages/learnearnreview.js
+++ www/js/pages/learnearnreview.js
@@ -1,34 +1,40 @@
 function page_learn_earn_main_review() {
-	// Add your JSON here 
-// 	getJson("", function(data) {
-// 		
-// 		var listString = '';
-// 		$.each(data, function (index, value) {
-// 			  listString += '<li data-psg-divider="' + learn and earn title+ '"> \
-// 					<div>' + Thank you or completion message + '</div> \
-// 					<div>\
-// 						<span>' + results of quiz or survey + '</span>\
-// 						<span>' + The completion date + '</span>\
-// 					</div>\
-// 					<div> <a href="" data-role="button" >back to learn and earn</a> </div> \
-// 				</li>';
-// 		});
-// 
-// 		var ul = $('#psg-listview-learnreview');
-// 		ul.html(listString);
-// 		ul.listview({
-// 			  autodividers: true,
-// 			  dividerTheme: "a" ,
-// 			  autodividersSelector: function (li) {
-// 				  var out = li.attr("data-psg-divider");
-// 				  return out;
-// 			  }
-// 		});
-// 		
-// 		ul.listview('refresh');
-// 		
-// 	  });
-// 	
-//    
+      var surveyId = sessionStorage.getItem('psg-learnearn-id');
+      var data = JSON.stringify({ surveyId: surveyId });
+      getJson("SURVEY.LIST.DETAIL", HandleSurveyDetailList, data);
 }
 
+
+function HandleSurveyDetailList(data) {
+     
+      var listString = '';
+    
+      listString += '<li data-psg-divider="' + data.SurveyTitle + '"> \
+      <div class="ui-no-ellipse psg-learnearn-li-section"><p>'+ data.CompletedMessage +'</p></div>\
+            <div class="ui-no-ellipse ui-text-small"><strong>Results: </strong>' + data.Score + '</div>\
+            <div class="ui-no-ellipse ui-text-small"><strong>Completed: </strong>' + data.dateCompleted + '</div>\
+      <div class=""> <button id="backLrnErn" class="ui-btn ui-btn-a ui-shadow ui-corner-all">Back to Learn & Earn</button> </div> \
+     </li>';
+
+      var leanEarnInstrc = $('.psg-learnearn-txt-content');
+      leanEarnInstrc.html(contentString);
+      
+      var ul = $('#psg-listview-learndetail');
+      ul.html(listString);
+      ul.listview({
+            autodividers: true,
+            dividerTheme: "a",
+            autodividersSelector: function (li) {
+                  var out = li.attr("data-psg-divider");
+                  return out;
+            }
+      });
+
+      ul.listview('refresh');
+      
+      $('#backLrnErn').on("click", function() {
+            $.mobile.changePage('learnearnmain.html'); 
+      });
+      
+  
+}
