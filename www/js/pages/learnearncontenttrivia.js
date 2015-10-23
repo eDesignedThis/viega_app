5201307bf801d4858d624e8d526fc3333ba891ee
Sheldon Bradley <sbradley@isicorporate.com>
2015-10-23 13:35:50 -0400

    Updated the Trivia content structure and removed the cheat button off of the home page.

--- www/js/pages/learnearncontenttrivia.js
+++ www/js/pages/learnearncontenttrivia.js
@@ -4,7 +4,6 @@
 	var surveyId = 0;
 	function page_learn_earn_content_trivia_show(){
 		var surveyId = sessionStorage.getItem('psg-learnearn-id'); 
-		//alert(surveyId);
 		var data = JSON.stringify({ surveyId: surveyId, counter: index, type: 'New', });  
 		getJson("SURVEY.TRIVIA.GET",HandleSurveyTrivia,data); 
 		
@@ -36,7 +35,6 @@
 	function HandleSurveyTrivia (data) {
 		var questionId = '';
 		var	questionType = '';	
-		debugger;
 		//Hide Buttons
 		ClearFields();
 
@@ -44,14 +42,65 @@
 			index = data.Count;
 			
 			if(index >= 0){
+				
 				HideShowControls(data);
-				if(data.DetailList[index].QuestionId != null){questionId = data.DetailList[index].QuestionId};
-				if(data.DetailList[index].QuestionType != null){questionType = data.DetailList[index].QuestionType};
-				if(data.DetailList[index].SurveyTitle != null){$('#lblSurveyTitle').text(data.DetailList[index].SurveyTitle)};
-				if(data.DetailList[index].QuestionHeader != null){$('#lblQuestionHeader').text(data.DetailList[index].QuestionHeader)};
-				if(data.DetailList[index].QuestionOrder != null){$('#lblSurveyNumber').text('Trivia Question #' + data.DetailList[index].QuestionOrder)};
-				if(data.DetailList[index].QuestionText != null){$('#lblSurveyQuestion').text(data.DetailList[index].QuestionText)};
-				if(data.Markup != null){$('#lblSurveyAnswers').html(data.Markup)};
+				
+				var listString = '';
+				
+				listString += '<li data-psg-divider="' + data.DetailList[index].SurveyTitle + '">\
+								<div class="ui-no-ellipse ui-text-small">'; // Open the div for trivia question number, question, and submit
+								
+				if(data.DetailList[index].QuestionHeader != null){
+					listString += '<p class="trivia_header" id="lblQuestionHeader">' + data.DetailList[index].QuestionHeader + '</p>';
+					}		
+					
+				if(data.DetailList[index].QuestionOrder != null){
+					listString += '<p class="trivia_question_number" id="lblSurveyNumber">Trivia Question #' + data.DetailList[index].QuestionOrder + '</p>';
+					}				
+				
+				if(data.DetailList[index].QuestionText != null){
+					listString += '<div class="trivia_question ui-no-ellipse ui-text-small">\
+										<p id="lblSurveyQuestion">' + data.DetailList[index].QuestionText + '</p>\
+									</div>'; 
+				}
+					listString += '<p class="error" id="lblError"></p>';
+					
+				if(data.Markup != null){
+					listString += '<div class="trivia_answer_container ui-no-ellipse ui-text-small">\
+										<p id="lblSurveyAnswers">' + data.Markup + '</p>\
+									</div>'; 
+				}	
+				
+				listString += '<div class="trivia_button_container ui-no-ellipse ui-text-small ui-grid-a">\
+									<div class="ui-block-a"><div class="ui-center"><p id="lblCorrect" class="trivia_correct_text"></p><p id="lblWrong" class="trivia_wrong_text"></p></div></div>\
+									<div class="ui-block-b"><div class="ui-center"><button data-mini="true" id="cmdSubmit" class="trivia_submit_button ui-btn ui-btn-a ui-shadow ui-corner-all" value="Submit" type="submit" >Submit</button></div></div>\
+								</div>\
+							</li></div>'; // Close the div for trivia question number, question, and submit			
+				
+				////// Pager section
+				
+				listString += '<li>\
+									<div id="pnlPager">\
+										<div class="trivia_pager_container ui-grid-a">\
+											<div class="ui-block-a"><div class="ui-center"><a href="#" id="cmdPrevious" class="trivia_pager_page_link trivia_pager_previous_page_link linkBtn ui-btn ui-btn-a ui-shadow ui-corner-all ui-mini">Prev</a></div></div>\
+											<div class="ui-block-b"><div class="ui-center"><a href="#" id="cmdNext" class="trivia_pager_page_link trivia_pager_next_page_link linkBtn ui-btn ui-btn-a ui-shadow ui-corner-all ui-mini">Next</a></div></div>\
+										</div>\
+									</div>\
+								</li>';
+				
+				
+				
+				// if(data.DetailList[index].QuestionId != null){questionId = data.DetailList[index].QuestionId};
+				// if(data.DetailList[index].QuestionType != null){questionType = data.DetailList[index].QuestionType};
+				// if(data.DetailList[index].SurveyTitle != null){$('#lblSurveyTitle').text(data.DetailList[index].SurveyTitle)};
+				// if(data.DetailList[index].QuestionHeader != null){$('#lblQuestionHeader').text(data.DetailList[index].QuestionHeader)};
+				// if(data.DetailList[index].QuestionOrder != null){$('#lblSurveyNumber').text('Trivia Question #' + data.DetailList[index].QuestionOrder)};
+				// if(data.DetailList[index].QuestionText != null){$('#lblSurveyQuestion').text(data.DetailList[index].QuestionText)};
+				// if(data.Markup != null){$('#lblSurveyAnswers').html(data.Markup)};
+				
+				
+				
+				
 			}
 			else
 			{
@@ -60,6 +109,21 @@
 				$('#cmdPrevious').show();								
 			}			
 		}
+		
+		var ul = $('#psg-listview-learntrivia');
+		ul.html(listString);
+		ul.listview({
+			autodividers: true,
+			dividerTheme: "a",
+			autodividersSelector: function (li) {
+				var out = li.attr("data-psg-divider");
+				return out;
+			}
+		});
+	
+		ul.listview('refresh');
+		
+		
 	}		
 	function HideShowControls(data)
 	{
@@ -144,7 +208,6 @@
 
 
 
-	
 		// Add your JSON here 
 // 		getJson("", function(data) {
 // 		
