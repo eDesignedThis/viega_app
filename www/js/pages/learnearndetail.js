c0941697f6d722f35ca3e3b386c0b494fdd99769
ckiechel <ckiechel@performancesystemsgroup.com>
2015-10-23 00:52:24 -0400

    Learn & Earn Detail - Worked out calls, data, and everything else needed to make it work.

--- www/js/pages/learnearndetail.js
+++ www/js/pages/learnearndetail.js
@@ -1,39 +1,53 @@
 function page_learn_earn_detail_show() {
-
-      var surveyId = sessionStorage.getItem('psg-learnearn-id');
-      //alert(surveyId);
-      var data = JSON.stringify({ surveyId: surveyId });
-      getJson("SURVEY.LIST.DETAIL", HandleSurveyDetailList, data);
+	var surveyId = sessionStorage.getItem('psg-learnearn-id');
+	var data = JSON.stringify({ surveyId: surveyId });
+	getJson("SURVEY.LIST.DETAIL", HandleSurveyDetailList, data);
 }
 
-
 function HandleSurveyDetailList(data) {
-      var listString = '';
-    
-      listString += '<li data-psg-divider="' + data.SurveyTitle + '"> \
-      <div class="ui-no-ellipse psg-learnearn-li-section"><p>'+ data.SurveyInstructions +'</p></div>\
-            <div class="ui-no-ellipse ui-text-small"><strong>Type: </strong>' + data.SurveyTypeText + '</div>\
-            <div class="ui-no-ellipse ui-text-small"><strong>Questions: </strong>' + data.questionCount + '</div>\
-            <div class="ui-no-ellipse ui-text-small psg-learnearn-li-section"><strong>Award: </strong>' + data.SurveyAward + '</div>\
-      <div class=""> <button id="" class="ui-btn ui-btn-a ui-shadow ui-corner-all" type="submit" >Start Now</button> </div> \
-     </li>';
+	if (psg.isNothing(data)) return;
 
-      var leanEarnInstrc = $('.psg-learnearn-txt-content');
-      leanEarnInstrc.html(contentString);
+	var listString = '';
+
+	listString += '<li data-psg-divider="' + data.SurveyTitle + '"> \
+		<div class="ui-no-ellipse ui-text-small"><p>' + data.SurveyInstructions + '</p></div>\
+	</li>\
+	<li>\
+		<div class="ui-no-ellipse ui-text-small"><p>' + data.SummaryText + '</p></div>\
+	</li>';
+		
+	if (!data.hasTaken && data.CanTake) {
+		listString += '<li><div class=""><button class="ui-btn ui-btn-a ui-shadow ui-corner-all" type="submit" >Start Now</button></div></li>';
+	}
+	else if (data.hasTaken && data.attempts <= data.Retakes && data.Score < data.SurveyPassingGrade && data.CanTake) {
+		if (data.attempts > 0) {
+			listString += '<li><a href="learnearncontent.html">Retake Now</a></li>';
+		}
+		else {
+			listString += '<li><a href="learnearncontent.html">Start Now</a></li>';
+		}
+	}
+	else if (data.hasTaken || data.attempts > data.Retakes) {
+		if (psg.isNothing(data.results)) {
+			listString += '<li><div class="ui-no-ellipse ui-text-small psg-learnearn-li-section"><p>Completed</p></div></li>';
+		} else {
+			listString += '<li><div class="ui-no-ellipse ui-text-small psg-learnearn-li-section"><p>Completed: ' + data.results + '</p></div></li>';
+		}
+		listString += '<li><a href="learnearncontent.html" data-direction="reverse" data-icon="arrow-l">Back</a></li>';
+	}
       
-      var ul = $('#psg-listview-learndetail');
-      ul.html(listString);
-      ul.listview({
-            autodividers: true,
-            dividerTheme: "a",
-            autodividersSelector: function (li) {
-                  var out = li.attr("data-psg-divider");
-                  return out;
-            }
-      });
+	var ul = $('#psg-listview-learndetail');
+	ul.html(listString);
+	ul.listview({
+		autodividers: true,
+		dividerTheme: "a",
+		autodividersSelector: function (li) {
+			var out = li.attr("data-psg-divider");
+			return out;
+		}
+	});
 
-      ul.listview('refresh');
-
+	ul.listview('refresh');
 };
   
 
