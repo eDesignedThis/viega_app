1114b128edfc83aab4dc0b5af49af23a8ae064a0
ckiechel <ckiechel@performancesystemsgroup.com>
2015-10-27 08:29:08 -0400

    Code cleanup and standard error handling on Learn and Earn main page.

--- www/js/pages/learnearnmain.js
+++ www/js/pages/learnearnmain.js
@@ -1,80 +1,85 @@
  function page_learn_earn_main_show(){
-	// Add your JSON here
-	getJson("SURVEY.LIST.GET",HandleSurveyAllList);
+	getJson("SURVEY.LIST.GET", HandleSurveyAllList);
 }
 
+function HandleSurveyAllList(data) {
+	if (psg.isNothing(data)) return;
+	if (data.Result == "success") {
+		DrawSurveyAllList(data.Surveys);
+	}
+	else {
+		WriteError(data.Error);
+	}
+}
 
-function HandleSurveyAllList (data) {	
+function DrawSurveyAllList (data) {	
 	
- 		/// Message on the learn and Earn main page
-		//var contentString = '';
-		//contentString += '<p>' + data.SurveyInstructions + '</p>';
-		//var learnEarnWelcome = $('.psg-learnearn-txt-content');
-      	//learnEarnWelcome.html(contentString);
+	// Message on the learn and Earn main page
+	//var contentString = '';
+	//contentString += '<p>' + data.SurveyInstructions + '</p>';
+	//var learnEarnWelcome = $('.psg-learnearn-txt-content');
+	//learnEarnWelcome.html(contentString);
 		 
- 		var listString = '';
- 		var endDate = '';
+	var listString = '';
+	var endDate = '';
 
+	if (psg.isNothing(data) || data.length == 0) {
+		listString += '<li><div class="ui-no-ellipse"><p>No opportunities to Learn-and-Earn are currently available.</p></div></li>';
+	}
+	else {
 		$.each(data, function (index, value) {
-			if (value.SurveyCategory=='Available Until:')
-			{
+			if (value.SurveyCategory=='Available Until:') {
 				endDate = moment(value.EndDate,'YYYY-MM-DD').format('MM-DD-YYYY')
 			}
 			else
 			{
 				endDate = '';	
 			}	
- 			  listString += '<li data-psg-divider="' + value.SurveyTypeText  + '"> \
- 				<a href="#" data-psg-learnearn-id="' + value.SurveyID + '" class="link-learnearn"> \
- 					<div class="ui-no-ellipse"><strong>' + value.SurveyTitle + '</strong></div> \
-					<div class="ui-text-small"> \
- 						<div class="ui-float-left"><strong>' + value.SurveyCategory + '</strong> ' + endDate + '</div> \
- 					</div> \
- 				</a> \
- 				</li>';
- 		});
+			listString += '<li data-psg-divider="' + value.SurveyTypeText  + '"> \
+			<a href="#" data-psg-learnearn-id="' + value.SurveyID + '" class="link-learnearn"> \
+				<div class="ui-no-ellipse"><strong>' + value.SurveyTitle + '</strong></div> \
+				<div class="ui-text-small"> \
+					<div class="ui-float-left"><strong>' + value.SurveyCategory + '</strong> ' + endDate + '</div> \
+				</div> \
+			</a> \
+			</li>';
+		});
+	}
  
- 		var ul = $('#psg-listview-learnearn');
- 		ul.html(listString);
- 		ul.listview({
- 			  autodividers: true,
- 			  dividerTheme: "a" ,
- 			  autodividersSelector: function (li) {
- 				  var out = li.attr("data-psg-divider");
- 				  return out;
- 			  }
- 		});
- 		ul.listview('refresh');
+	var ul = $('#psg-listview-learnearn');
+	ul.html(listString);
+	ul.listview({
+		autodividers: true,
+		dividerTheme: "a" ,
+		autodividersSelector: function (li) {
+			var out = li.attr("data-psg-divider");
+			return out;
+		}
+	});
+	ul.listview('refresh');
 		 
- 		$('.link-learnearn').on("click", function() {
- 				sessionStorage.setItem('psg-learnearn-id', $(this).attr('data-psg-learnearn-id'));
-				 /// check if survey/quiz or trivia and if survey/quiz is completed or available
-				 var learnEarnSelected = $(this).closest('li').attr('data-psg-divider');
-				 var learnEarnCatgry = $(this).find('div.ui-float-left strong').html();
+	$('.link-learnearn').on("click", function() {
+		sessionStorage.setItem('psg-learnearn-id', $(this).attr('data-psg-learnearn-id'));
+		// check if survey/quiz or trivia and if survey/quiz is completed or available
+		var learnEarnSelected = $(this).closest('li').attr('data-psg-divider');
+		var learnEarnCatgry = $(this).find('div.ui-float-left strong').html();
 				 
-				 console.log(learnEarnSelected);
-				  console.log(learnEarnCatgry);
-				 
+		//console.log(learnEarnSelected);
+		//console.log(learnEarnCatgry);
 	
-				 if(learnEarnSelected == 'Trivia'){
-					 $.mobile.changePage('learnearncontenttrivia.html');
-				 } else{
-					 console.log("This is not Trivia");
-					 if(learnEarnCatgry == 'Completed'){
-						 console.log("Check the category again here: " + learnEarnCatgry);
-						 console.log("The staus is available");
-						 $.mobile.changePage('learnearnreview.html');
-					 }else {
-						 console.log("Check the category again here: " + learnEarnCatgry);
-						console.log("The staus is available");
-						$.mobile.changePage('learnearndetail.html');
-						 
-					 }
-					 
-				 }
- 				
- 			});
- 	 
- 	
-        
+		if (learnEarnSelected == 'Trivia'){
+			$.mobile.pageContainer.pagecontainer('change', 'learnearncontenttrivia.html', { transition: 'slide', changeHash: true } );
+		} else {
+			//console.log("This is not Trivia");
+			if (learnEarnCatgry == 'Completed'){
+				//console.log("Check the category again here: " + learnEarnCatgry);
+				//console.log("The staus is available");
+				$.mobile.pageContainer.pagecontainer('change', 'learnearnreview.html', { transition: 'slide', changeHash: true } );
+			} else {
+				//console.log("Check the category again here: " + learnEarnCatgry);
+				//console.log("The staus is available");
+				$.mobile.pageContainer.pagecontainer('change', 'learnearndetail.html', { transition: 'slide', changeHash: true } );
+			}
+		}
+	});
  }
