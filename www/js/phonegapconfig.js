babb1fad101819bcb721a86e14159481c9a99a46
Bobby Strickland <bstrickland@incentivesolutions.com>
2018-03-05 16:13:38 -0500

    Fiberon Setup

--- www/js/phonegapconfig.js
+++ www/js/phonegapconfig.js
@@ -1,11 +1,11 @@
 var config = {
-appName: 'RewardTrax',
+appName: 'FiberonDeckDollars',
 appPushId: 'AA322-266A3',
 //this is the project number not the project Id
 googleProjectNumber: '66243742706', 
 msServiceName: '',
-host: 'https://www.rewardtrax.com/brandeddemo15',
-base: '/brandeddemo15/m/',
+host: 'https://fiberon.rewardtrax.com',
+base: '/m/',
 googleUA: 'UA-61738744-1'
 
 }
