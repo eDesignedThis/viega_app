b5beca22343b3a9fd8b6ea113195bc5f506805e5
administrator <kheld@performancesystemsgroup.com>
2018-04-02 13:52:20 -0400

    Revert "Fiberon Setup"
    This reverts commit babb1fad101819bcb721a86e14159481c9a99a46.

--- www/js/phonegapconfig.js
+++ www/js/phonegapconfig.js
@@ -1,11 +1,11 @@
 var config = {
-appName: 'FiberonDeckDollars',
+appName: 'RewardTrax',
 appPushId: 'AA322-266A3',
 //this is the project number not the project Id
 googleProjectNumber: '66243742706', 
 msServiceName: '',
-host: 'https://fiberon.rewardtrax.com',
-base: '/m/',
+host: 'https://www.rewardtrax.com/brandeddemo15',
+base: '/brandeddemo15/m/',
 googleUA: 'UA-61738744-1'
 
 }
