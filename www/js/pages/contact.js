594c015df29b444e83a3005bf4b1e61dd2c5f7c0
Terry Thorne <Terry Thorne>
2020-02-02 19:09:58 -0500

    Merged PR 1039: Contacts #12170 Contact Us
    Contacts #12170 add Configurable  items to contact us subject and email.
    
    Related work items: #12170

--- www/js/pages/contact.js
+++ www/js/pages/contact.js
@@ -4,18 +4,40 @@ function page_contact_show(){
         $('#contact_submit').show();
         $('#contact_result').text('');
 		
+		var useDefaults = '0';
+		var $xml = $(psg.configXml);
+
+		if ($xml.find('CONTACTS[USE_DEFAULTS="1"]').length == 1){
+			useDefaults = '1';
+		}	
+		
 		var options = '';
-		if (psg.payoutType != '1') {
-			options = '<option value="Catalog Question">Catalog Question</option> \
-                    <option value="My Shopping Order">My Shopping Order</option> \
-                    <option value="Track an Order">Track an Order</option> \
-                    <option value="Return an Item">Return an Item</option>';
-		} else {
-			options = '<option value="My Prepaid Debit Card">My Prepaid Debit Card</option>';
-		}
-        options +=	'<option value="An Award Correction">An Award Correction</option> \
-						<option value="Program Question">Program Question</option> \
-						<option value="Other">Other</option>';
+		if(useDefaults == '1'){
+			if (psg.payoutType != '1') {
+				options = '<option value="Catalog Question">Catalog Question</option> \
+						<option value="My Shopping Order">My Shopping Order</option> \
+						<option value="Track an Order">Track an Order</option> \
+						<option value="Return an Item">Return an Item</option>';
+				} else {
+					options = '<option value="My Prepaid Debit Card">My Prepaid Debit Card</option>';
+				}
+				options +=	'<option value="An Award Correction">An Award Correction</option> \
+							<option value="Program Question">Program Question</option>';
+			
+			}
+
+		//debugger;
+		var useConfiguredOptions = '0';
+		$xml.find('CONTACTS > CONTACT').each(function(index){
+			useConfiguredOptions = '1';
+           var id = $(this)[0].attributes.CONTACT_SUBJECT_ID;
+		   var textValue = $(this)[0].attributes.CONTACT_SUBJECT_NAME;
+			options +=	'<option value="' + id.value + '">' + textValue.value + '</option>';
+        });
+	
+		options +=	'<option value="Other">Other</option>';
+		$('#use-configuration-options').val(useConfiguredOptions); // hidden field 
+		
 		$('#contact_subject').html(options);				
         $('#contact_subject').change( function () {
             var other_subject_container = $('#contact_other_subject_container');
@@ -33,7 +55,7 @@ function page_contact_show(){
 }
 
  function submitContactForm() {
-      var data = JSON.stringify({ subject: $('#contact_subject').val(), other: $('#contact_other_subject').val(), question: $('#contact_question').val() });
+      var data = JSON.stringify({ subject: $('#contact_subject').val(), other: $('#contact_other_subject').val(), question: $('#contact_question').val(), use_config: $('#use-configuration-options').val() });
       getJson("CONTACT.CONTACTUSPOST", HandleContactSubmit, data);
       
         //Send tracking data to Google
