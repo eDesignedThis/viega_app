0e111e9686a94cb1d1da4a87d28d5ed7ec0cf2cc
Erika Wright <erikawright@administrators-MacBook-Pro.local>
2020-04-30 19:25:36 -0400

    registered app with apple

--- www/js/phonegapconfig.js
+++ www/js/phonegapconfig.js
@@ -1,11 +1,10 @@
 var config = {
-appName: 'HYShareRewards',
-appPushId: 'D3EF2-35712',
-//this is the project number not the project Id
-googleProjectNumber: '646856504251', 
-msServiceName: '',
-host: 'https://www.hysharerewards.com',
-base: '/m/',
-googleUA: 'UA-61738744-29'
-
-}
+  appName: "MasterTech Rewards",
+  appPushId: "D3EF2-35712",
+  //this is the project number not the project Id
+  googleProjectNumber: "646856504251",
+  msServiceName: "",
+  host: "https://mastertechrewards.com",
+  base: "/m/",
+  googleUA: "UA-61738744-29",
+};
